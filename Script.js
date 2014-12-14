fbe9371e5a6a9f2821f7e32b766508cce0d7c4bd
LeoSsss0315 <light0315@gmail.com>
2014-12-14 01:36:28 -0500

    Testing

---
+++ Script.js
@@ -0,0 +1,6 @@
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='Index.html'>Home</a></li>" +
+"<li><a href='Customers.html'>Data</a></li>" +
+"<li><a href='About.html'>About</a></li>" +
+"</ul>"; 
