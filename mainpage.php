fae82fa5a95c62b17664986dbf98ff613de79b03
LeoSsss0315 <light0315@gmail.com>
2014-12-14 01:34:29 -0500

    Try to create a login page

---
+++ mainpage.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>PHP Demo</title>
+<link href="Site.css" rel="stylesheet">
+</head>
+<body>
+
+<nav id="nav01"></nav>
+
+<div id="main">
+  <h1>Welcome to Our Company</h1>
+  <h2>Web Site Main Ingredients:</h2>
+
+  <p>Pages (HTML)</p>
+  <p>Style Sheets (CSS)</p>
+  <p>Computer Code (JavaScript)</p>
+  <p>Live Data (Files and Databases)</p>
+
+  <footer id="foot01"></footer>
+</div>
+
+<script src="Script.js"></script>
+
+</body>
+</html> 
